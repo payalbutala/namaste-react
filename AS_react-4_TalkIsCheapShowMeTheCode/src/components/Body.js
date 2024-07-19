0d4012f0ec3fe16546b9bbe69b9bb262fce9fec2
pbutala <pbutala@tree.com>
2024-07-19 19:08:23 +0530

    AS 6 in-progress

--- AS_react-4_TalkIsCheapShowMeTheCode/src/components/Body.js
+++ AS_react-4_TalkIsCheapShowMeTheCode/src/components/Body.js
@@ -1,10 +1,32 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
   // Super Variable - super powerful variable
-  const [listOfRestaurants, setListOfRestaurant] = useState(resList); // state variable by React, [] inside useState is default value;
+  let [listOfRestaurants, setListOfRestaurant] = useState([]); // state variable by React, [] inside useState is default value;
+
+  // useEfefct = simply a normal Javascript Function, has 2 arguments (callback function, dependency array)
+  // When will this useEffect callback function will be called? => after component renders. If you want to do something after component render, use useEffect.
+  useEffect(() => {
+    // console.log("useEffect called");
+    fetchData();
+  }, []);
+
+  // console.log("Body Component Rendered");
+
+  const fetchData = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    
+    const json = await data.json();
+    console.log(json);
+
+    setListOfRestaurant(
+      listOfRestaurants = json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      // optional chaining is to add => ?
+
+    );
+  }
+
 
   /* let listOfRestaurants = [];
   function abc() {
@@ -13,6 +35,12 @@ const Body = () => {
       return item.info.avgRating > 4;
     });
   } */
+
+    if (listOfRestaurants.length === 0) {
+      return <h1>Loading....</h1>;
+    } else {
+      
+    }
 
   return (
     <div className="body">
